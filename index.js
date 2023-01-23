95d7487248bf357367f0f29e0da720836690eb54
Dhawaj Kumar Tomar <77123160+Dhawaj1999@users.noreply.github.com>
2023-01-23 23:24:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 // detecting button key
 // we are using loop to access every button for drum
-for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
-
+for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-
     // this helps in selecting the keyword of button and changing its property
     var button = this.innerHTML;
-
     makeSound(button);
     animation(button);
   });
+// detect keyboard button and play sound using makesound() function
+  document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    animation(event.key);
+  });
 }
 
-// play sound based on button press
+
+// play sound based on button click
 function makeSound(key){
   switch (key) {
 
   case "w":
-    // this fucntionality we are using in javascript to play sound using HTML audio element
+  // this fucntionality we are using in javascript to play sound using HTML audio element
     var crash = new Audio("sounds/crash.mp3");
     crash.play();
     break;
 
   case "a":
-    var kick = new Audio("sounds/kick-bass.mp3");
-    kick.play();
+    var kickbass = new Audio("sounds/kick-bass.mp3");
+    kickbass.play();
     break;
 
   case "s":
@@ -51,25 +54,14 @@ function makeSound(key){
     var tom4 = new Audio("sounds/tom-4.mp3");
     tom4.play();
     break;
-
-  default:
-
 }
 }
 
-// detect keyboard button and play sound using makesound() function
-document.addEventListener("keydown", function(event){
-  makeSound(event.key);
-  animation(event.key);
-});
-
-// ANIMATION
+// Animation
 function animation(keyNow){
   var buttonPressed = document.querySelector("." + keyNow);
-
   buttonPressed.classList.add("pressed");
-
   setTimeout(function(){
-      buttonPressed.classList.remove("pressed");
-  }, 100);
+  buttonPressed.classList.remove("pressed");
+}, 100);
 }
